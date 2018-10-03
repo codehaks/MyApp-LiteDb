25d875de8a071ea4da5f61e3997be4cf56e5817c
Hakim Ghods <ghods.hakim@hotmail.com>
2018-10-04 00:07:53 +0330

    Add project files.

---
+++ MyApp/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Codehaks.Extentions.LiteDb;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MyApp
+{
+    public class Startup
+    {
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddLiteDb("app.db");
+        }
+
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
